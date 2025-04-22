3d4bf95211f4374e78891a772cae3d185881a3ea
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-04-23 02:01:29 +0530

    message

---
+++ skill_sharing_platform/src/test/java/com/project/skill_sharing_platform/SkillSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.skill_sharing_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
