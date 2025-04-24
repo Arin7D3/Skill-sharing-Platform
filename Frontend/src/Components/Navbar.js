7fc0ef020eabba4a4ecd09c43eeafd5ffdad8a1c
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-04-24 12:17:05 +0530

    added

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
       <h1>Aris</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
-        <li className="hghlight"><Link to="/posts">Posts</Link></li>
+        <li className="highlight"><Link to="/posts">Posts</Link></li>
         <li>About Us</li>
       </ul>
     </nav>
