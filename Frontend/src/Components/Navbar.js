2ea0ccd6ea37ae180492043e024e275503937bea
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-04-26 01:08:01 +0530

    added Navbar.js

--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -7,7 +7,11 @@ function Navbar() {
     <nav className="navbar">
       <h1>Aris</h1>
       <ul>
+<<<<<<< Updated upstream
         <li><Link to="/">Home</Link></li>
+=======
+        <li><Link to="/">Home Page</Link></li>
+>>>>>>> Stashed changes
         <li className="highlight"><Link to="/posts">Posts</Link></li>
         <li>About Us</li>
       </ul>
