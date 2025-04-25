4d25a543380095ce2434d7058e38d6789174e7fd
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-04-26 01:33:40 +0530

    ADDED LoginResponse & LoginRequest DTO files

---
+++ Backend/src/main/java/com/project/skill_sharing_platform/dto/LoginResponse.java
@@ -0,0 +1,52 @@
+package com.project.skill_sharing_platform.dto;
+
+import java.time.LocalDateTime;
+
+public class LoginResponse {
+
+public class LoginResponseDTO {
+    private String token;
+    private LocalDateTime time;
+    private String error;
+    private String message;
+
+    public LoginResponseDTO(String token, LocalDateTime time, String error, String message) {
+        this.token = token;
+        this.time = time;
+        this.error = error;
+        this.message = message;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+}
