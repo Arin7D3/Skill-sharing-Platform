215f90df040dd4b0e7f6bbaef1e465898a693c30
Arin <152803463+Arin7D3@users.noreply.github.com>
2025-04-27 00:59:05 +0530

    changed SecurityConfig file

--- Backend/src/main/java/com/project/Config/SecurityConfig.java
+++ Backend/src/main/java/com/project/Config/SecurityConfig.java
@@ -1,14 +1,11 @@
 package com.project.Config;
 
-import com.project.Service.CustomOAuth2UserService;
+import com.project.skill_sharing_platform.Service.CustomOAuth2UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-
-
 
 @Configuration
 public class SecurityConfig {
@@ -31,6 +28,4 @@ public class SecurityConfig {
                 )
                 .build();
     }
-}
-
 }
